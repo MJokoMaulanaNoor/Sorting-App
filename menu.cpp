111e031d28d0c9a131ff9559c4b8c75656ccdcdf
MJokoMaulanaNoor <mjokomaulananoor@gmail.com>
2024-12-18 01:16:41 +0800

    penyesuaian isi pada halaman depan

--- menu.cpp
+++ menu.cpp
@@ -2,62 +2,105 @@
 #include <conio.h>
 using namespace std;
 
+// Variabel global
+int n;
+int data[100];
 
-void dMenu1(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+// Fungsi tukar
+void tukar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+// Fungsi untuk input data
+void masukkanData() {
+    system("cls");
+    cout << "Masukkan jumlah data: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Masukkan data ke-" << i + 1 << ": ";
+        cin >> data[i];
+    }
+    cout << "Data berhasil dimasukkan.";
+    getch();
 }
 
+// Fungsi untuk menampilkan data
+void tampilkanData() {
+    system("cls");
+    cout << "Data: ";
+    for (int i = 0; i < n; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+    getch();
+}
+
+// Fungsi sorting ascending
+void sortingAsc() {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (data[j] > data[j + 1]) {
+                tukar(&data[j], &data[j + 1]);
+            }
+        }
+    }
+    cout << "Data telah diurutkan secara ascending.";
+    getch();
+}
+
+// Fungsi sorting descending
+void sortingDsc() {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (data[j] < data[j + 1]) {
+                tukar(&data[j], &data[j + 1]);
+            }
+        }
+    }
+    cout << "Data telah diurutkan secara descending.";
+    getch();
+}
+
+// Menu utama
+void dMenu() {
+    system("cls");
+    cout << "Aplikasi Sorting Bubble" << "\n";
+    cout << "1. masukkan data" << "\n";
+    cout << "2. tampilkan data" << "\n";
+    cout << "3. sorting asc" << "\n";
+    cout << "4. sorting dsc" << "\n";
+    cout << "5. Exit" << "\n";
+    cout << "Masukan angka :";
+}
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
-
-
-} while (pl!='5');
-  return 0;
+    char pl;
+    do {
+        dMenu();
+        pl = getch();
+        switch (pl) {
+        case '1':
+            masukkanData();
+            break;
+        case '2':
+            tampilkanData();
+            break;
+        case '3':
+            sortingAsc();
+            break;
+        case '4':
+            sortingDsc();
+            break;
+        case '5':
+            break;
+        default:
+            system("cls");
+            cout << "Pilihan Tidak Tersedia";
+            getch();
+            break;
+        }
+    } while (pl != '5');
+    return 0;
 }
